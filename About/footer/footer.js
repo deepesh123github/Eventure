d93fbedbd50bdfcbf530601528ad722cd1b692a8
deepesh123github <133794619+deepesh123github@users.noreply.github.com>
2025-03-12 11:31:21 +0530

    Delete About directory

--- About/footer/footer.js
+++
@@ -1,4 +0,0 @@
-fetch('footer/footer.html')  // Corrected path to the footer file
-.then(response => response.text())
-.then(data => document.getElementById('footer').innerHTML = data)
-.catch(error => console.error('Error loading footer:', error)); // Added error handling
